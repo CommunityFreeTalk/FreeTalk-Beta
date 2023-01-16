30d48423f8c1f9f45a04fc835490684f88082407
cutegyuseok <yigyuseok@gmail.com>
2023-01-16 19:11:39 +0900

    create

---
+++ src/main/java/com/example/freetalk/FreeTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.freetalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/freetalk/FreeTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freetalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
