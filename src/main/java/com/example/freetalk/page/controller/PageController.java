7f9c728daba13b1dc8fd33145d62093eebf6565f
hamings <wjdgpal9630@gmail.com>
2023-01-25 21:58:47 +0900

    feat: add CommunityPage

--- src/main/java/com/example/freetalk/page/controller/PageController.java
+++ src/main/java/com/example/freetalk/page/controller/PageController.java
@@ -2,20 +2,39 @@ package com.example.freetalk.page.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 @Controller
 @RequiredArgsConstructor
 public class PageController {
 
+//    @GetMapping("/")
+//    public String index(){
+//        return "index";
+//    }
+
+
+
     @GetMapping("/socialLogin")
     public String loginPage(){
         return "login";
     }
+    @GetMapping("/search")
+    @ResponseBody
+    public String search(@RequestParam String keyword){
+        String[] keywords  = keyword.split(" ");
+        return keyword;
 
+    }
+
+    @GetMapping("/community")
+    public String community(){
+        return "community";
+    }
 }
