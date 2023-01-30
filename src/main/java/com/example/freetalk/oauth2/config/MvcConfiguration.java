5467d00c1f3941af529c2eed67286f9449149ae6
hamings <wjdgpal9630@gmail.com>
2023-01-30 13:40:25 +0900

    feat: image upload function

---
+++ src/main/java/com/example/freetalk/oauth2/config/MvcConfiguration.java
@@ -0,0 +1,16 @@
+package com.example.freetalk.oauth2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/contentsImgs/**")
+                .addResourceLocations("file:C:\\fileUpload");
+    }
+
+}
+
+
