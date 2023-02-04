332c6003c598098ae2fbbd36f9dd4ea947db5749
cutegyuseok <yigyuseok@gmail.com>
2023-02-04 22:26:08 +0900

    fix: search service interface 구축

--- src/main/java/com/example/freetalk/community/service/impl/SearchServiceImpl.java
+++ src/main/java/com/example/freetalk/community/service/impl/SearchServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.freetalk.community.service.impl;
 
 import com.example.freetalk.community.dto.ResultGroupDTO;
 import com.example.freetalk.community.repository.SearchRepository;
+import com.example.freetalk.community.service.SearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +12,11 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class SearchService {
+public class SearchServiceImpl implements SearchService {
 
     private final SearchRepository sr;
 
+    @Override
     public List<ResultGroupDTO> selectByHashTag(String[] keywords){
         List<String> kws = Arrays.asList(keywords);
         List<ResultGroupDTO> list = sr.selectByHashTagList(kws);
